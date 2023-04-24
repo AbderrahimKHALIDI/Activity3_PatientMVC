4afa0716cdbf0446bdc67dba31aa302b1e64807f
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-04-24 23:00:49 +0200

    Complete1

--- src/main/java/ma/emsi/patientmvc/web/PatientController.java
+++ src/main/java/ma/emsi/patientmvc/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,13 +19,33 @@ public class PatientController {
 
     private PatientRepositorie patientRepositorie;
 @GetMapping("/index")
-    public String patients(Model model, @RequestParam(name="page",defaultValue = "0") int page , @RequestParam(name="size",defaultValue = "5") int size ){
-    Page<Patient> pagePatients=patientRepositorie.findAll(PageRequest.of(page,size));
+    public String patients(Model model,
+                           @RequestParam(name="page",defaultValue = "0") int page ,
+                           @RequestParam(name="size",defaultValue = "5") int size,
+                           @RequestParam(name="keyword",defaultValue = "") String keyword){
+    Page<Patient> pagePatients=patientRepositorie.findByNomContains(keyword,PageRequest.of(page,size));
     model.addAttribute("ListPatients",pagePatients.getContent());
     model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
     model.addAttribute("currentPage",page);
+    model.addAttribute("keyword",keyword);
     return "patients";
 
     }
+    @GetMapping("/delete")
+    public String delete(Long id, String keyword,int page){
+    patientRepositorie.deleteById(id);
+    return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public String home(){
 
+        return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+
+        return patientRepositorie.findAll();
+    }
 }
