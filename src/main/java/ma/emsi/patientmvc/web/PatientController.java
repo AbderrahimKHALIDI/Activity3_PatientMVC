4dda695e5246fafd13cd2246058f0c711e1a747f
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-04-24 14:07:10 +0200

    Update

--- src/main/java/ma/emsi/patientmvc/web/PatientController.java
+++ src/main/java/ma/emsi/patientmvc/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.emsi.patientmvc.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.patientmvc.entities.Patient;
 import ma.emsi.patientmvc.repositories.PatientRepositorie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,11 @@ public class PatientController {
 
     private PatientRepositorie patientRepositorie;
 @GetMapping("/index")
-    public String patients(Model model,){
-    List<Patient> patients=patientRepositorie.findAll();
-    model.addAttribute("ListPatients",patients);
+    public String patients(Model model, @RequestParam(name="page",defaultValue = "0") int page , @RequestParam(name="size",defaultValue = "5") int size ){
+    Page<Patient> pagePatients=patientRepositorie.findAll(PageRequest.of(page,size));
+    model.addAttribute("ListPatients",pagePatients.getContent());
+    model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+    model.addAttribute("currentPage",page);
     return "patients";
 
     }
